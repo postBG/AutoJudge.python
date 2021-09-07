fcf00f2a5432177dac468a1e80d055038ba31c97
postBG <profile2697@gmail.com>
2021-09-08 03:04:19 +0900

    add i/o using file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 def main():
-    project_root = "/Users/postbg/IdeaProjects/JavaTest"
+    project_root = "/Users/postbg/IdeaProjects/DataStructureAssignment/Assignment1"
     source_root = f"{project_root}/src"
     compile_path = f"{project_root}/out"
     target_path = "problem1"
@@ -12,11 +12,28 @@ def main():
     target_filename = f"{target_name}.java"
     target_file_path = os.path.join(source_root, target_path, target_filename)
 
+    assigment_example_path = "/Users/postbg/PycharmProjects/AutoJudge.python/assigment1"
+    inputs_path = f"{assigment_example_path}/inputs"
+    answers_path = f"{assigment_example_path}/answers"
+    inputs = read_file(inputs_path)
+    answers = read_file(answers_path)
+
     proc = compile_code(target_file_path, compile_path)
     proc.communicate()
-    proc = run_code(source_root, compile_path, target_path, target_name)
-    out = proc.communicate()[0]
-    print(out == b'5\n')
+    for file_name, args in inputs.items():
+        proc = run_code(source_root, compile_path, target_path, target_name, args)
+        out = proc.communicate()[0]
+        print(f'{file_name} result: {out == answers[file_name]}')
+
+
+def read_file(inputs_path):
+    input_example_files = os.listdir(inputs_path)
+    inputs = {}
+    for input_example_file in input_example_files:
+        with open(os.path.join(inputs_path, input_example_file), 'rb') as f:
+            args = f.read().replace(b'\r', b'').lstrip(b'\n')
+            inputs[input_example_file] = args
+    return inputs
 
 
 def compile_code(file_path, compile_path):
@@ -24,11 +41,11 @@ def compile_code(file_path, compile_path):
     return proc
 
 
-def run_code(project_path, compile_path, target_path, target_name):
+def run_code(project_path, compile_path, target_path, target_name, inputs):
     classpath = os.path.join(project_path, compile_path)
     target_class = '.'.join([target_path, target_name])
     proc = Popen(['java', '-classpath', classpath, target_class], stdin=PIPE, stdout=PIPE)
-    proc.stdin.write(b'5')
+    proc.stdin.write(inputs)
     return proc
 
 
