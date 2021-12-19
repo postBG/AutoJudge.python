017bbee2b68a597a0a5ddb916be4f050ba1176ca
postBG <profile2697@gmail.com>
2021-12-19 19:31:05 +0900

    fix config

--- test_cases/__init__.py
+++ test_cases/__init__.py
@@ -1,5 +1,5 @@
 from test_cases.file_test_cases import SimpleFileTestCases
-from test_cases.manual_test_cases import Assignment0ManualTestCases
+from test_cases.manual_test_cases import Assignment0ManualTestCases, Assignment3ManualTestCases
 
 
 def test_cases_factory(configs):
@@ -8,5 +8,7 @@ def test_cases_factory(configs):
         return SimpleFileTestCases(configs.assigment_testcase_root)
     elif code == "assignment0_manual":
         return Assignment0ManualTestCases()
+    elif code == 'assignment3_manual':
+        return Assignment3ManualTestCases()
     else:
         raise ValueError(f"{code} is not supported.")
